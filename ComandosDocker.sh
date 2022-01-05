228efc52cd9488b8f19926e783a56f482fbb8541
SergiodelaMata <sergio.matam@edu.uah.es>
2022-01-05 18:54:11 +0100

    :sparkles: Corrected example test project.

---
+++ ComandosDocker.sh
@@ -0,0 +1,15 @@
+docker run -d -p 6379:6379 --name redisservice redis:6.0.9-alpine
+
+docker network create mongodb-net
+docker run -d -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=password --name mongodb --net mongodb-net mongo
+docker run -d -p 8082:8081 -e ME_CONFIG_MONGODB_ADMINUSERNAME=admin -e ME_CONFIG_MONGODB_ADMINPASSWORD=password -e ME_CONFIG_MONGODB_SERVER=mongodb --name mongo-express --net mongodb-net mongo-express
+
+docker logs mongo-express
+docker-compose up -f mongo-docker-compose.yaml
+docker-compose down -f mongo-docker-compose.yaml
+
+docker stop $(docker ps -aq)
+
+docker login -u sergiomata99
+docker image tag aplicacion sergiomata99/aplicacion
+docker push sergiomata99/aplicacion
--- index.js
+++ index.js
@@ -5,22 +5,35 @@ const redis = require('redis');
 const PORT = process.env.PORT || 5000;
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
-const client = redis.createClient(REDIS_PORT);
 const app = express();
+
+var client = redis.createClient(REDIS_PORT);
+
+startConnectionRedis();
+
+async function startConnectionRedis()
+{
+    if(!client.isOpen)
+    {
+        client = redis.createClient(REDIS_PORT);
+        await client.connect();
+    }
+}
 
 async function getRepos(req, res, next) {
     try {
-        console.log("Haciendo el fecch de la API de GitHub");
+        console.log("Haciendo el fetch de la API de GitHub");
         const {username} = req.params;
         const response = await fetch(`https://api.github.com/users/${username}`);
 
         const data = await response.json();
 
         const repos = data.public_repos;
-        client.setEx(username,repos); //Clave, tiempo para expiración (1 hora), datos a guardar
+        saveMongo(username,repos);
 
-        res.send(setResponse(username, repos));
-
+        await client.set(username, repos); //Clave, tiempo para expiración (1 hora), datos a guardar
+        await client.disconnect();
+        res.send(formateaSalida(username, repos));
         console.log("Fin de la petición a la API de GitHub");
     } catch (err) {
         console.error(err);
@@ -28,26 +41,66 @@ async function getRepos(req, res, next) {
     }
 }
 
-function setResponse(username, repos){
-    return `<h2>El usuario ${username} tiene ${repos} repositorios en GitHub </h2>`
+function formateaSalida(username, repos){
+    return `<h4>El usuario ${username} tiene ${repos} repositorios en GitHub </h4>`
 }
 
-function cacheo(req, res, next){
+
+async function cacheo(req, res, next){
+    startConnectionRedis()
     console.log("Testeando si el dato está en caché");
     const {username} = req.params;
-    client.get(username, (err, data) => {
+    const data = await client.get(username);
+    if(data !== null)
+    {
+        console.log("Caché hit!");
+        client.disconnect();
+        res.send(formateaSalida(username, data));
+    }
+    else{
+        console.log("Caché miss");
+        next();
+    }
+}
+
+const MongoClient = require('mongodb').MongoClient;
+
+async function saveMongo(usuario, repositorio){
+
+    clientemongo = MongoClient.connect('mongodb://admin:password@localhost:27017',
+    function(err, clientemongo){
         if(err) throw err;
-        if(data !== null){
-            console.log("Caché hit!");
-            res.send(setResponse(username, data));
-        }
-        else{
-            console.log("Caché miss");
-            next();
-        }
+        
+        const db = clientemongo.db('repositorio');
+        const query = {nombre:usuario};
+        const datosnuevos = { $set: {'repos': repositorio}};
+        db.collection('usuarios').findOneAndUpdate(query, datosnuevos, {upsert:true}, function(err, resultado){
+            if(err) throw err;
+            clientemongo.close();
+            console.log(resultado);
+        });
     });
 }
 
+function testMongo(req, res, next){
+    console.log('testMongo');
+    MongoClient.connect('mongodb://admin:password@localhost:27017',
+    function (err, clientemongo){
+        if(err) throw err;
+
+        const db = clientemongo.db('repositorio');
+        const {username} = req.params;
+        const query = {nombre: username};
+        db.collection('usuarios').findOne(query, function(err, resultado){
+            if(err) throw err;
+            clientemongo.close();
+            res.send(resultado);
+            console.log(resultado);
+        });
+    });
+}
+
+app.get("/datosbasededatos/:username", testMongo);
 app.get("/datosrepositorio/:username", cacheo, getRepos);
 
 app.listen(PORT, () => {
