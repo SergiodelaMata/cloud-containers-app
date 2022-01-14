f1450df534d95fb9c68952d64cc33d9ffc14adce
SergiodelaMata <sergio.matam@edu.uah.es>
2022-01-15 00:22:04 +0100

    :sparkles: Map the response received.

--- src/server/routes/front/routes.ts
+++ src/server/routes/front/routes.ts
@@ -2,57 +2,72 @@ import express, { Request, Response, Router } from "express";
 import config from "../../../../config";
 import fetch from "node-fetch";
 import {Ports} from "../../server.ports";
+import { GetProducts, GetProduct} from "../../../interfaces/product.interface";
+import { GetUsers, GetUser} from "../../../interfaces/user.interface";
+import { GetTransactions, GetTransaction} from "../../../interfaces/transaction.interface";
+import { GetHome} from "../../../interfaces/home.interface";
 
 
 const router: Router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render(`${config.rootFolder}/src/views/home.ejs`);
+  const response = {
+    logged : false,
+    userId : false,
+    admin : null,
+  }
+  const formattedResponse: GetProducts = JSON.parse(JSON.stringify(response));
+  res.render(`${config.rootFolder}/src/views/home.ejs`, formattedResponse);
 })
 
 router.get("/products", async(_req: Request, res: Response) => {
   const response = await fetch(`http://localhost:${Ports.Enrouting + _req.url}`);
   const productData = await response.json();
-  res.render(`${config.rootFolder}/src/views/products.ejs`, productData);
+  const formattedResponse: GetProducts = JSON.parse(JSON.stringify(productData));
+  res.render(`${config.rootFolder}/src/views/products.ejs`, formattedResponse);
 })
 
 router.get("/products/:productId", async(_req: Request, res: Response) => {
   const response = await fetch(`http://localhost:${Ports.Enrouting + _req.url}`);
   const productData = await response.json();
-  console.log(productData);
-  res.render(`${config.rootFolder}/src/views/detailsProduct.ejs`, productData);
+  const formattedResponse: GetProduct = JSON.parse(JSON.stringify(productData));
+
+  res.render(`${config.rootFolder}/src/views/detailsProduct.ejs`, formattedResponse);
 })
 
 router.get("/comprar", (req, res) => {
-  res.render(`${config.rootFolder}/src/views/comprar.ejs`);
+  const response = {
+    logged : false,
+    userId : false,
+    admin : null,
+  }
+  const formattedResponse: GetProducts = JSON.parse(JSON.stringify(response));
+  res.render(`${config.rootFolder}/src/views/comprar.ejs`, formattedResponse);
 })
 
 router.get("/vender", (req, res) => {
-  res.render(`${config.rootFolder}/src/views/vender.ejs`);
+  const response = {
+    logged : false,
+    userId : false,
+    admin : null,
+  }
+  const formattedResponse: GetProducts = JSON.parse(JSON.stringify(response));
+  res.render(`${config.rootFolder}/src/views/vender.ejs`, formattedResponse);
 })
 
-router.get("/movies", (req, res) => {
-  res.status(200).send(
-    [{ ok: true }, { ok: true }, { ok: true }]
-  );
-});
+router.get("/users", async(_req: Request, res: Response) => {
+  const response = await fetch(`http://localhost:${Ports.Enrouting + _req.url}`);
+  const userData = await response.json();
+  const formattedResponse: GetUsers = JSON.parse(JSON.stringify(userData));
+  res.render(`${config.rootFolder}/src/views/users.ejs`, formattedResponse);
+})
 
-router.get("/movies/:movie", (req, res) => {
-  res.status(200).send(
-    { ok: req.params.movie }
-  );
-});
 
-router.post("/movie", (req, res) => {
-  res.status(200).send(
-    req.body
-  );
-});
-
-router.put("/movie/:movie", (req, res) => {
-  res.status(200).send(
-    {body: req.body, id: req.params.movie}
-  );
+router.delete("/admin/user/:userId", async(req: Request, res: Response) => {
+  const response = await fetch(`http://localhost:${Ports.Enrouting + req.url}`, {
+    method:"delete",
+  });
+  res.send(await response.json());
 });
 
 
